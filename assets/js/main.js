d316cb9b870e502a25d4abd8b93d5e8071fe1f2c
Mirko <mirko.rinelli@gmail.com>
2021-01-08 19:11:15 +0100

    Working on selector in API

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,32 @@
 let app = new Vue ({
-  el: "#app"
-}) 
+  el: '#app',
+  data: {
+    emailContacts: [
+      {
+        name: 'Mirko',
+        email: ''
+      },
+      {
+        name: 'Greta',
+        email: ''
+      },
+      {
+        name: 'Franco',
+        email: ''
+      },
+      {
+        name: 'Giulio',
+        email: ''
+      }
+    ]
+  },
+  mounted(){
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then(resp => {
+      console.log(resp.data.response);
+      let apiEmail = resp.data.response;
+      this.email.email = apiEmail;
+      console.log(this.emailContacts.email);
+    })
+  }
+});
