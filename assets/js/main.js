2b110786640653f711cdd666888ecbd87a91ccf6
Mirko <mirko.rinelli@gmail.com>
2021-01-10 17:04:27 +0100

    Print list of emails

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,15 @@
 let app = new Vue ({
   el: '#app',
   data: {
-    emailContacts: [
-      {
-        name: 'Mirko',
-        email: ''
-      },
-      {
-        name: 'Greta',
-        email: ''
-      },
-      {
-        name: 'Franco',
-        email: ''
-      },
-      {
-        name: 'Giulio',
-        email: ''
-      }
-    ]
+    emailContacts: [],
   },
   mounted(){
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then(resp => {
-      console.log(resp.data.response);
-      let apiEmail = resp.data.response;
-      this.email.email = apiEmail;
-      console.log(this.emailContacts.email);
-    })
+    for (let i = 0; i < 10; i++) {
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+      .then(resp => {
+        console.log(resp.data.response);
+        this.emailContacts.push(resp.data.response)
+      })
+    }
   }
 });
